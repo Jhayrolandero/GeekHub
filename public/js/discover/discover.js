7a8bd6b744b998f6bdcbf0d8e2d5769b1bf81521
xjaylandero <xjaylandero@gmail.com>
2023-11-24 09:23:33 +0800

    Added like and comment in discover page

--- public/js/discover/discover.js
+++ public/js/discover/discover.js
@@ -28,4 +28,149 @@ $(document).ready(function () {
       }
     );
   }
+
+  /* 
+  =============
+  
+  Like System
+
+  =============
+  */
+
+  // Like
+  $("#discover-post").on("click", ".like-btn", function () {
+    var groupPostID = $(this)
+      .closest(".community-post-card")
+      .find(".community-post-id")
+      .val();
+    var userID = $(this).closest(".community-post-card").find(".user-id").val();
+    // var communityID = $(this)
+    //   .closest(".community-post-card")
+    //   .find(".community-id")
+    //   .val();
+
+    $.post(
+      "app/controller/CommunityController.php",
+      {
+        action: "likeCommmunityPost",
+        groupPostID: groupPostID,
+        userID: userID,
+      },
+      function (data, status) {
+        if (status === "success") {
+          render_dicover_page();
+        } else {
+          alert("Error!");
+        }
+      }
+    );
+  });
+
+  // Unlike
+  $("#discover-post").on("click", ".unlike-btn", function () {
+    var groupPostID = $(this)
+      .closest(".community-post-card")
+      .find(".community-post-id")
+      .val();
+    var userID = $(this).closest(".community-post-card").find(".user-id").val();
+    var communityID = $(this)
+      .closest(".community-post-card")
+      .find(".community-id")
+      .val();
+
+    $.post(
+      "app/controller/CommunityController.php",
+      {
+        action: "unlikeCommmunityPost",
+        groupPostID: groupPostID,
+        userID: userID,
+      },
+      function (data, status) {
+        if (status === "success") {
+          render_dicover_page();
+        } else {
+          alert("Error!");
+        }
+      }
+    );
+  });
+
+  /*
+  ====================
+  COMMENT
+  ====================
+  */
+
+  function renderComment(groupPostID) {
+    $.get(
+      "app/controller/CommunityController.php",
+      {
+        action: "showCommunityComment",
+        groupPostID: groupPostID,
+      },
+      function (data, status) {
+        if (status === "success") {
+          $("#comment-list").html(data);
+        }
+      }
+    );
+  }
+  // Open Comment
+  $("#discover-post").on("click", ".comment-btn", function () {
+    $(".community-comment-modal").slideDown();
+
+    var groupPostID = $(this)
+      .closest(".community-post-card")
+      .find(".community-post-id")
+      .val();
+
+    var targetID = $(this)
+      .closest(".community-post-card")
+      .find(".creator_id")
+      .val();
+
+    var groupID = $(this)
+      .closest(".community-post-card")
+      .find(".community-id")
+      .val();
+
+    $(".comment-post-id").val(groupPostID);
+    $(".comment-target-id").val(targetID);
+    $(".comment-community-id").val(groupID);
+
+    renderComment(groupPostID);
+  });
+
+  // CLose comment modal
+  $(".close-btn").click(function () {
+    $(".community-comment-modal").slideUp();
+  });
+
+  // Comment
+
+  $("#add-comment-btn").click(function () {
+    var comment = $(".comment-input").val();
+    var groupPostID = $(".comment-post-id").val();
+    var userID = $(".comment-user-id").val();
+    // var groupID = $(".comment-community-id").val();
+
+    $.post(
+      "app/controller/CommunityController.php",
+      {
+        action: "addCommentCommmunityPost",
+        comment: comment,
+        groupPostID: groupPostID,
+        userID: userID,
+      },
+      function (data, status) {
+        if (status === "success") {
+          // Update contents dynamically
+          renderComment(groupPostID);
+          console.log(data);
+        } else {
+          alert("Error! Try again");
+        }
+      }
+    );
+  });
 });
